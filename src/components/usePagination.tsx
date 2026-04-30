e3e3d23c2544c4542814038e8165937157ecd108
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-30 15:34:21 -0400

    added calculations for total pages and for getting the indexes for items show on the page

--- src/components/usePagination.tsx
+++ src/components/usePagination.tsx
@@ -7,11 +7,22 @@ type UsePaginationProps = {
 }
 
 function usePagination({
-    totalItems,
+    totalItems = 100,
     itemsPerPage = 10,
     initialPage = 1,
 }: UsePaginationProps) {
     const [currentPage, setCurrentPage] = useState(initialPage);
+
+    const totalPages = Math.ceil(totalItems / itemsPerPage); // CALCULATE HOW MANY PAGES ARE NEEDED FOR 100 ITEMS
+
+    // CALCULATE INDEXES FOR PAGES
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage -1;
+
+    // ITEMS ON CURRENT PAGE
+    
+
+    return {};
 }
 
 export default usePagination;
