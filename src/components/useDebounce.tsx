5f4d700fe297e0252ff5e523637790c6d35af7e5
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-01 15:09:07 -0400

    set a timeout in useEffect to start when the debounced value changes and to stop if the value changes before the timer is done

--- src/components/useDebounce.tsx
+++ src/components/useDebounce.tsx
@@ -4,6 +4,19 @@ function useDebounce<T>(value: T, delay: number = 500) {
     const [debounceValue, setDebounceValue] = useState(value);
 
     useEffect(() => {
+        // VALUE CHANGES START A TIMER
+        const timer = setTimeout(() => {
+            setDebounceValue(value)
+        }, delay)
+
+        // CLEAR TIMER
+        return () => {
+           clearTimeout(timer)
+        }
 
     }, [value, delay]);
+
+    return debounceValue;
 }
+
+export default useDebounce;
