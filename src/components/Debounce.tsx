8eaa66053a443406a6c061e465cb5c2a1646aa03
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-30 21:01:48 -0400

    added UI basic structure

---
+++ src/components/Debounce.tsx
@@ -0,0 +1,14 @@
+function Debounce() {
+    return (
+        <div>
+            <p>Debounce Delay (ms): </p>
+            <input type="text" placeholder="Type to search..." />
+            <p>Debounced Value (after 600ms):</p>
+            <br />
+            <p>Simulated Search Results: </p>
+            <p>Type to see results.</p>
+        </div>
+    )
+}
+
+export default Debounce;
