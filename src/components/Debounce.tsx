e81ba2468a0f26199b4618013b7ec9202967b4c7
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-01 14:25:00 -0400

    added p element for displaying current input

--- src/components/Debounce.tsx
+++ src/components/Debounce.tsx
@@ -2,9 +2,9 @@ function Debounce() {
     return (
         <div>
             <p>Debounce Delay (ms): </p>
-            <input type="text" placeholder="Type to search..." />
+            <input type="text" placeholder="Type to search..." className="debounce-search" />
+            <p>Current Input</p>
             <p>Debounced Value (after 600ms):</p>
-            <br />
             <p>Simulated Search Results: </p>
             <p>Type to see results.</p>
         </div>
