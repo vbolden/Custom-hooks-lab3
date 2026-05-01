8b0d8223820a352fcaf0a9ca0616bfe6e065cdbe
Valerie Bolden <valerie.bolden@gmail.com>
2026-05-01 14:35:23 -0400

    created a debounce useState assigned to value initially; added a useEffect with value and delay as dependencies

--- src/components/useDebounce.tsx
+++ src/components/useDebounce.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
 
 function useDebounce<T>(value: T, delay: number = 500) {
-    
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+
+    }, [value, delay]);
 }
